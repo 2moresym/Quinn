29f50236ec0641fa593c78fb72ceeb69dd523fc4
2vex <2nosym@gmail.com>
2026-09-09 21:29:21 -0500

    feat: initialize Quinn pre-generated voice player

--- quinn-daemon/src/main.rs
+++ quinn-daemon/src/main.rs
@@ -1,5 +1,6 @@
 mod app_classifier;
 mod apps;
+mod audio;
 mod commands;
 mod reminders;
 mod service;
@@ -57,8 +58,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
             None
         }
     };
+    let voice_player = Arc::new(audio::VoicePlayer::new());
 
-    let daemon = service::QuinnDaemon::new(engine, apps, classifier, voice);
+    let daemon = service::QuinnDaemon::new(engine, apps, classifier, voice, voice_player);
     let _connection = connection::Builder::session()?
         .name("org.quinn.Assistant")?
         .serve_at("/org/quinn/Assistant", daemon)?
