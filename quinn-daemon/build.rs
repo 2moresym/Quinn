86dc4a74fcd796dd77c04c60d238b1568278029b
2vex <2nosym@gmail.com>
2026-09-09 22:40:54 -0500

    fix: force native Vosk link path and fail clearly

--- quinn-daemon/build.rs
+++ quinn-daemon/build.rs
@@ -20,11 +20,18 @@ fn main() {
         .into_iter()
         .flatten()
         .find(|path| path.join("libvosk.so").is_file())
-        .unwrap_or_else(|| PathBuf::from("../native/vosk"));
+        .unwrap_or_else(|| {
+            panic!(
+                "could not find libvosk.so; set QUINN_VOSK_LIB_DIR or install it at ~/.local/share/quinn/vosk"
+            )
+        });
 
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
     println!("cargo:rustc-link-lib=dylib=vosk");
 
     #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir.display());
+    {
+        println!("cargo:rustc-link-arg=-L{}", lib_dir.display());
+        println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir.display());
+    }
 }
